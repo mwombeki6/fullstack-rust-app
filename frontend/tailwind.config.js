f0323b1737d70fb5bf49ec72aebdbb87574b2e5e
mwombeki <hackerlubex@protonmail.com>
2023-06-29 00:00:02 +0300

    commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{rs,html}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins, sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          lg: '1125px',
+          xl: '1125px',
+          '2xl': '1125px',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
 
